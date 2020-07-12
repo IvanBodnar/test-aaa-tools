c0f2e911b724ba0c0a1fb5879972f20bd6623d52
ivan <ibodnar@outlook.com>
2020-07-12 11:16:56 -0300

    created: RequestsClient

---
+++ src/ipc/http_client.py
@@ -0,0 +1,19 @@
+from urllib.parse import urljoin
+
+import requests
+from requests import Response
+from requests.exceptions import RequestException
+
+from .exceptions import RequestsError
+
+
+class RequestsClient:
+    def __init__(self, base_url: str = ''):
+        self._base_url = base_url
+
+    def send(self, url: str, method: str = 'GET', **kwargs) -> Response:
+        url = urljoin(self._base_url, url)
+        try:
+            return requests.request(method=method, url=url, **kwargs)
+        except RequestException as e:
+            raise RequestsError(str(e))
