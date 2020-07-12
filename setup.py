e92fbb9e8b01b7c9d1e53e77d92ce215cd788db9
ivan <ibodnar@outlook.com>
2020-07-12 11:06:50 -0300

    initial commit

--- setup.py
+++ setup.py
@@ -1,12 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='python-template',
+    name='test-aaa-tools',
     version='0.1',
     package_dir={'': 'src'},
-    packages=find_packages(where='src'),
-    entry_points='''
-        [console_scripts]
-        run=src.main:run
-    ''',
+    packages=find_packages(where='src')
 )
